0489842e6ccc96cba8e1834199b68180ac6173ec
eason <eason@DESKTOP-608BVMD>
2017-07-19 14:38:16 +0800

    修改商品place属性

--- paimai/src/main/java/com/zking/pojo/Goods.java
+++ paimai/src/main/java/com/zking/pojo/Goods.java
@@ -1,11 +1,13 @@
 package com.zking.pojo;
 
+import java.util.Arrays;
+
 public class Goods {
     private Integer goodsId;
 
     private String goodsName;
 
-    private String goodsPlace;
+    private Integer goodsPlace;
 
     private Integer goodstypeId;
 
@@ -16,6 +18,26 @@ public class Goods {
     private String godsInfo;
 
     private byte[] goodsPicture;
+    
+    
+    public Goods() {
+		// TODO Auto-generated constructor stub
+	}
+    
+    
+    
+    public Goods(Integer goodsId, String goodsName, Integer goodsPlace, Integer goodstypeId, Integer uid,
+    		Integer goodsStatus, String godsInfo, byte[] goodsPicture) {
+    	super();
+    	this.goodsId = goodsId;
+    	this.goodsName = goodsName;
+    	this.goodsPlace = goodsPlace;
+    	this.goodstypeId = goodstypeId;
+    	this.uid = uid;
+    	this.goodsStatus = goodsStatus;
+    	this.godsInfo = godsInfo;
+    	this.goodsPicture = goodsPicture;
+    }
 
     public Integer getGoodsId() {
         return goodsId;
@@ -33,11 +55,11 @@ public class Goods {
         this.goodsName = goodsName;
     }
 
-    public String getGoodsPlace() {
+    public Integer getGoodsPlace() {
         return goodsPlace;
     }
 
-    public void setGoodsPlace(String goodsPlace) {
+    public void setGoodsPlace(Integer goodsPlace) {
         this.goodsPlace = goodsPlace;
     }
 
@@ -80,4 +102,20 @@ public class Goods {
     public void setGoodsPicture(byte[] goodsPicture) {
         this.goodsPicture = goodsPicture;
     }
+
+
+
+	@Override
+	public String toString() {
+		return "Goods [goodsId=" + goodsId + ", goodsName=" + goodsName + ", goodsPlace=" + goodsPlace
+				+ ", goodstypeId=" + goodstypeId + ", uid=" + uid + ", goodsStatus=" + goodsStatus + ", godsInfo="
+				+ godsInfo + ", goodsPicture=" + Arrays.toString(goodsPicture) + "]";
+	}
+
+    
+    
+    
+    
+    
+    
 }
